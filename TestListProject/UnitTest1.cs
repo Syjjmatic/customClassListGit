67743d9e274088490f4a85384e05f3559ab8e975
Andrew Curzon <andrew.w.curzon@gmail.com>
2020-02-24 10:58:09 -0600

    got three test methods set up and created the starter add method

--- TestListProject/UnitTest1.cs
+++ TestListProject/UnitTest1.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using customClassList;
 
 namespace TestListProject
 {
@@ -7,8 +8,50 @@ namespace TestListProject
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void AddObjectsToListTest_CountIncreased()
         {
+            CustomList<int> myList = new CustomList<int>();
+            int valueToAdd = 10;
+            int expected = 1;
+            int actual;
+
+            myList.AddToList(valueToAdd);
+            actual = myList.Count;
+
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void AddObjectsToListTest_TheFirstItemIndexIsZero()
+        {
+            CustomList<int> myList = new CustomList<int>();
+            int valueToAdd = 10;
+            int expected = 10;
+            int actual;
+
+            myList.AddToList(valueToAdd);
+            actual = myList[0];
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void AddObjectsToListTest_TheSecondItemIndexIsOne()
+        {
+            CustomList<int> myList = new CustomList<int>();
+            int valueToAdd = 10;
+            int valueToAdd1 = 15;
+            int expected = 15;
+            int actual;
+
+            myList.AddToList(valueToAdd);
+            myList.AddToList(valueToAdd1);
+            actual = myList[1];
+
+            Assert.AreEqual(expected, actual);
+
+
         }
     }
 }
