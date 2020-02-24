9e687e2396653fa38c8533e7f76240ba4fa8b990
Andrew Curzon <andrew.w.curzon@gmail.com>
2020-02-24 11:08:16 -0600

    Added two test methods that test for the array size of the list and the string of the item in the first index.

--- TestListProject/UnitTest1.cs
+++ TestListProject/UnitTest1.cs
@@ -18,7 +18,6 @@ namespace TestListProject
             myList.AddToList(valueToAdd);
             actual = myList.Count;
 
-
             Assert.AreEqual(expected, actual);
         }
 
@@ -50,8 +49,41 @@ namespace TestListProject
             actual = myList[1];
 
             Assert.AreEqual(expected, actual);
+        }
 
+        [TestMethod]
+        public void AddObjectsToListTest_CorrectArraySize()
+        {
+            CustomList<int> myList = new CustomList<int>();
+            int valueToAdd = 10;
+            int valueToAdd1 = 15;
+            int valueToAdd2 = 20;
+            int valueToAdd3 = 25;
+            int valueToAdd4 = 30;
+            int expected = 8;
+            int actual;
 
+            myList.AddToList(valueToAdd);
+            myList.AddToList(valueToAdd1);
+            myList.AddToList(valueToAdd2);
+            myList.AddToList(valueToAdd3);
+            myList.AddToList(valueToAdd4);
+            actual = myList.Length;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        public void AddObjectsToListTest_FirstItemHasCorrectName()
+        {
+            CustomList<string> myList = new CustomList<string>();
+            string name = "Andrew";
+            string expected = "Andrew";
+            string actual;
+
+            myList.AddToList(name);
+            actual = myList[0];
+
+            Assert.AreEqual(expected, actual);
         }
     }
 }
